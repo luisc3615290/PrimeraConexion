171ec670a846a40ed8f156a8c4c540a467acb9b0
luisc3615290 <3615290@alu.murciaeduca.es>
2024-10-01 22:04:06 +0200

    Hola Mundo 01/10/2024 22:04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Ejemplo de cambio en la máquina local que veremos en github");
     }
 }
